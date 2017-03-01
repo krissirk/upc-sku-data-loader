44354e514a1d642a6101d65055870f4365e6959e
Kris Harrison <kris_harrison@gap.com>
2017-02-28 16:14:06 -0800

    Build full script
    1.) Finalized MySQLdb approach + connection method
    2.) Assembled INSERT statement
    3.) Added initialization and setting of brand code for each insert
    4.) Execute query logic + error handling
    5.) Process all pages of product catalog response for all legacy brands
    6.) Added logic for processing single entity brands as commented out
    code

--- load-upc-data.py
+++ load-upc-data.py
@@ -1,4 +1,4 @@
-import requests, json, time, os, sys #, MySQLdbt
+import requests, json, time, os, sys, MySQLdb
 from config import *
 
 # Get key value required to access Product Catalog API from environment variable set by secret shell script and assemble header for request; exit if variable not set
@@ -9,17 +9,12 @@ else:
 	print "Environment variable not set - cannot proceed"
 	sys.exit(2)
 
-# myoffers.io MySQL db credentials 
-dbPort = MYSQL_PORT
-dbSchema = MYSQL_DATABASE
-dbHost = MYSQL_HOST
-dbUser = MYSQL_USER
-dbPassword = MYSQL_PASSWORD
-
-##################### FUNCTION DEFINITIONS #####################
+######################################### FUNCTION DEFINITIONS #########################################
 
 # Function that iterates through API resopnse to insert each relevant SKU and associated UPC code to myoffers.io database
-def insertSkus(styles, type):
+def insertSkus(styles, type, brandCode, cursor):
+
+	sqlValues = ""
 
 	for items in styles:						# Iterate through each style in the response
 
@@ -29,17 +24,32 @@ def insertSkus(styles, type):
 				
 				if type == "legacy":			# If processing 'legacy' business units, work with Store UPC value; else, work with online UPC
 
-					if "storeUPC" in skus:			
+					if "storeUPC" in skus:		# If a Store UPC value exists for the SKU, append the details to the VALUES portion of the INSERT statement
 						
-						upc = skus["storeUPC"]
-						skuId = skus["businessId"]
+						sqlValues += "({0},{1},{2}), ".format(skus["businessId"], skus["storeUPC"], brandCode)
 
 				elif type == "singleEntity":
 
-					upc = skus["onlineUPC"]
-					skuId = skus["businessId"]
+					sqlValues += "({0},{1},{2}), ".format(skus["businessId"], skus["onlineUPC"], brandCode)
 
-				# Insert SKU into database
+	# Build the full INSERT statement with complete set of VALUES to upload (trim last 2 characters of VALUES string to get rid of trailing comma and space)
+	sqlStatement = "INSERT INTO upc_KH (SKU,UPC,Brand) VALUES {0} ON DUPLICATE KEY UPDATE UPC = VALUES (UPC);".format(sqlValues[:-2])
+
+	# Try/Catch execution of the MySQL INSERT statement
+	try:
+
+		# Commit changes to the MySQL database and commit
+		cursor.execute(sqlStatement)
+		db.commit()						
+
+	except:
+
+		# Rollback if there is an error
+		db.rollback()
+		db.close()
+		
+		print "Database error: ", time.asctime( time.localtime(time.time()) )
+		sys.exit(2)
 
 	return
 
@@ -59,16 +69,20 @@ def apiRequest(url, key):
 
 	return apiResponse
 
-##################### END OF FUNCTION DEFINITIONS ####################
+######################################### END OF FUNCTION DEFINITIONS #########################################
 
 print "Start: ", time.asctime( time.localtime(time.time()) )	#Log script start time to console
 
-# Initialization of tuple containing Product Catalog paths of all online US business units
-# Legacy brands will need to be handled differently than single entity brands as single entity brands do not have store UPCs
-legacyBizUnits = ("br/us", "gp/us", "on/us")
-singleEntityBizUnits = ("at/us", "brfs/us", "gapfs/us")
+# Initialization of dictionary containing Product Catalog paths of all online US business units and associated brand code
+# Legacy brands will need to be handled differently than Single Entity brands as SE brands do not have Store UPCs that are distinct from Online UPCs
+legacyBizUnits = {"br/us": 2, "gp/us": 1, "on/us": 3}
+singleEntityBizUnits = {"at/us": 10, "brfs/us": 6, "gpfs/us": 5}
 
-# Process each of the *legacy* business units
+# Set MySQL connection string from config file containing database information and open conncetion
+db = MySQLdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)
+dbCursor = db.cursor()
+
+# Process each of the *Legacy* business units
 for bizUnit in legacyBizUnits:
 
 	# Product Catalog API url to access ALL products for a business unit, including the SKUs
@@ -76,38 +90,71 @@ for bizUnit in legacyBizUnits:
 
 	# Initial Product Catalog API request - this gets the first batch of products to be processed and determines how many total pages need to be iterated through
 	catalogResponse = apiRequest(initialApiUrl, apiKey)
-	pages = catalogResponse.json()["page"]["totalPages"]			# Grab total number of pages in Product Catalog API response
+	pages = catalogResponse.json()["page"]["totalPages"]				# Grab total number of pages in Product Catalog API response
 	print "Total pages to process for {0}: ".format(bizUnit), pages		# Log total number of pages that need to be processed to the console
 
-	# Check the initial response for SKU of interest
-	insertSkus(catalogResponse.json()["_embedded"]["styles"], "legacy")	
-	print "1 page of SKUs uploaded"
+	# Process initial page of SKUs for insertion to MySQL db
+	insertSkus(catalogResponse.json()["_embedded"]["styles"], "legacy", legacyBizUnits[bizUnit], dbCursor)	
+	print "1 page of SKUs uploaded for {0}".format(bizUnit)
 
-sys.exit(2)
+	# Grab URL of 'next' pagination link in Product Catalog response to process during first iteration of while loop
+	nextLink = catalogResponse.json()["_links"]["next"]["href"]
 
+	x = 1	# Initialize counter for while loop that will ensure the entire Product Catalog is processed
 
-# Check the initial response for SKU of interest
-insertSkus(catalogResponse.json()["_embedded"]["styles"])	
-print "1 page uploaded"
+	# Process all remaining pages of Product Catalog response
+	while x < pages:
 
-# Grab URL of 'next' pagination link in Product Catalog response to process during first iteration of while loop
-nextLink = catalogResponse.json()["_links"]["next"]["href"]
+		# Make next request of Product Catalog and check the resulting response for SKU of interest
+		catalogResponse = apiRequest(nextLink, apiKey)
+		insertSkus(catalogResponse.json()["_embedded"]["styles"], "legacy", legacyBizUnits[bizUnit], dbCursor)
 
-x = 1	# Initialize counter for while loop that will ensure the entire Product Catalog is processed
+		# Grab URL of 'next' pagination link for subsequent request until the data element is no longer in the response (which will only happen during final iteration of loop)
+		if "next" in catalogResponse.json()["_links"]:
+			nextLink = catalogResponse.json()["_links"]["next"]["href"]
 
-# Process all remaining pages of Product Catalog response
-while x < pages:
+		# Increment counter & log progress to console
+		x += 1
+		print x, "pages of SKUs uploaded for {0}".format(bizUnit)
 
-	# Make next request of Product Catalog and check the resulting response for SKU of interest
-	catalogResponse = apiRequest(nextLink, apiKey)
-	insertSkus(catalogResponse.json()["_embedded"]["styles"])
+# Process each of the *Single Entity* business units
+"""
+for bizUnit in singleEntityBizUnits:
 
-	# Grab URL of 'next' pagination link for subsequent request until the data element is no longer in the response (which will only happen during final iteration of loop)
-	if "next" in catalogResponse.json()["_links"]:
-		nextLink = catalogResponse.json()["_links"]["next"]["href"]
+	# Product Catalog API url to access ALL products for a business unit, including the SKUs
+	initialApiUrl = "https://api.gap.com/commerce/product-catalogs/catalog/{0}?&size=333&includeSkus=true".format(bizUnit)
 
-	# Increment counter & log progress to console
-	x += 1
-	print x, "pages processed"
+	# Initial Product Catalog API request - this gets the first batch of products to be processed and determines how many total pages need to be iterated through
+	catalogResponse = apiRequest(initialApiUrl, apiKey)
+	pages = catalogResponse.json()["page"]["totalPages"]				# Grab total number of pages in Product Catalog API response
+	print "Total pages to process for {0}: ".format(bizUnit), pages		# Log total number of pages that need to be processed to the console
+
+	# Process initial page of SKUs for insertion to MySQL db
+	insertSkus(catalogResponse.json()["_embedded"]["styles"], "singleEntity", singleEntityBizUnits[bizUnit], dbCursor)	
+	print "1 page of SKUs uploaded for {0}".format(bizUnit)
+
+	# Grab URL of 'next' pagination link in Product Catalog response to process during first iteration of while loop
+	nextLink = catalogResponse.json()["_links"]["next"]["href"]
+
+	x = 1	# Initialize counter for while loop that will ensure the entire Product Catalog is processed
+
+	# Process all remaining pages of Product Catalog response
+	while x < pages:
+
+		# Make next request of Product Catalog and check the resulting response for SKU of interest
+		catalogResponse = apiRequest(nextLink, apiKey)
+		insertSkus(catalogResponse.json()["_embedded"]["styles"], "singleEntity", singleEntityBizUnits[bizUnit], dbCursor)
+
+		# Grab URL of 'next' pagination link for subsequent request until the data element is no longer in the response (which will only happen during final iteration of loop)
+		if "next" in catalogResponse.json()["_links"]:
+			nextLink = catalogResponse.json()["_links"]["next"]["href"]
+
+		# Increment counter & log progress to console
+		x += 1
+		print x, "pages of SKUs uploaded for {0}".format(bizUnit)
+"""
+
+# Disconnect from MySQL server
+db.close()
 
 print "End: ", time.asctime( time.localtime(time.time()) )	# Log script completion ending time to console
